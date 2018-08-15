cf44f0b7d5c47d844daedb4a0ec4b290f3b1d167
Prathibha Sathyajith <prathibha4you@gmail.com>
2018-08-15 14:46:29 +0530

    test from pycham

--- mSite/crypto.py
+++ mSite/crypto.py
@@ -10,6 +10,7 @@ from keyset import keyProcess
 
 f = keyProcess()
 location = "storeSite/static/keyfile/000000001000066.pem"
+
 print("{} -- {}".format("Byte Signed Data String",f.signingProcess(f.loadPemfile(location), "000000001000066")));
 print("{}           -- {}".format("Encrypted mid", f.encryptionPrecess("000000001000066", f.generateSharedKey(location))))
 
